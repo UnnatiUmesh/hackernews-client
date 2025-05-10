420b6cdb34ad0df1627a7ad0818a444f703c5aa4
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-05-10 15:34:02 +0530

    Refactor better-auth integration to use environment variable for baseURL

--- app/lib/integrations/better-auth/index.ts
+++ app/lib/integrations/better-auth/index.ts
@@ -2,8 +2,9 @@ import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from "better-auth/client/plugins";
 
+const API_BASE=process.env.NEXT_PUBLIC_API_BASE_URL;
 export const betterAuthClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: API_BASE,
   plugins: [nextCookies(), usernameClient()],
 });
 
